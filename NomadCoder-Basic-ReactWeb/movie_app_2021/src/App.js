7e2a6d97220671511c29e9b30fe8f111bd645851
kr-MATAGI <dev_matagi@kakao.com>
2021-01-31 17:17:09 +0900

    add router

--- NomadCoder-Basic-ReactWeb/movie_app_2021/src/App.js
+++ NomadCoder-Basic-ReactWeb/movie_app_2021/src/App.js
@@ -1,61 +1,14 @@
 import React from 'react';
-import axios from 'axios'
-import Movie from './Movie';
-import "./App.css";
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home'
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    //https://yts-proxy.nomadcoders1.now.sh/list_movies.json
-    const {
-      data: {
-        data: {
-          movies
-        }
-      }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  }
-
-  renderMovies = (movies) => {
-      return (movies.map(movie => {
-        return (<Movie 
-          key={movie.id}
-          id={movie.id}
-          year={movie.year}
-          title={movie.title}
-          summary={movie.summary}
-          poster={movie.medium_cover_image}
-          genres={movie.genres}
-          />);
-      }));
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? ( 
-          <div>
-            <span className="loader_text">
-              Loading...
-            </span>
-          </div>) : (
-            <div className="movies">
-              {this.renderMovies(movies)}
-            </div>
-          )}
-      </section>
-    );
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
+function App() {
+  return (<HashRouter>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
+  );
 }
 
 export default App;
