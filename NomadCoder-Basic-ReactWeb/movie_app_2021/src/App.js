0fa64167d84e8aec1341ddf339486442542ce1b5
kr-MATAGI <dev_matagi@kakao.com>
2021-01-31 18:47:39 +0900

    add redrecting

--- NomadCoder-Basic-ReactWeb/movie_app_2021/src/App.js
+++ NomadCoder-Basic-ReactWeb/movie_app_2021/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Details from "./routes/Details";
 import Navigation from './components/Navigation';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
     <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Details}/>
   </BrowserRouter>
   );
 }
--- NomadCoder-Basic-ReactWeb/movie_app_2021/src/components/Movie.js
+++ NomadCoder-Basic-ReactWeb/movie_app_2021/src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
@@ -16,17 +17,28 @@ const renderGenres = (genres) => {
 
 function Movie({id, year, title, summary, poster, genres}){
     return (
-        <div className="movie">
-            <img src={poster} title={title} alt={title} />
-            <div className="movie_data">
-                <h3 className="movie_title">{title}</h3>
-                <h5 className="movie_year">{year}</h5>
-                <ul className="movie_genres">
-                    {renderGenres(genres)}
-                </ul>
-                <p className="movie_summary">{summary.slice(0,140)}...</p>
+        <Link to={{
+            pathname:"/movie/${id}",
+            state:{
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
+            <div className="movie">
+                <img src={poster} title={title} alt={title} />
+                <div className="movie_data">
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <ul className="movie_genres">
+                        {renderGenres(genres)}
+                    </ul>
+                    <p className="movie_summary">{summary.slice(0,140)}...</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
