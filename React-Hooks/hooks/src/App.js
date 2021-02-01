117d8b1a031caf0ae3990fb6dd117a2b562adb36
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 20:28:46 +0900

    useScroll

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
-const useNetwork = (onChange) => {
-  const [state, setState] = useState(navigator.online);
-  const handleChange = () => {
-    if (typeof onChange === 'function') {
-      onChange(navigator.online);
-    }
-    setState(navigator.online);
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const onScroll = () => {
+    setState({
+      x: window.scrollX,
+      y: window.scrollY
+    });
   };
   useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
+    window.addEventListener("scroll", onScroll);
 
-    return () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
+
   return state;
 };
 
 const App = () => {
-  const handleNetworkChage = online => {
-    console.log(online ? "online" : "offline");
-  };
-  var online = useNetwork(handleNetworkChage);
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1>{online ? "Online" : "Offline"}</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
     </div>
   );
 };
