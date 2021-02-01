ce3763a23cd38e47222b4db4c10c8082aa0ecd5e
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 17:39:00 +0900

    useConfirm

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 const useConfirm = (msg, confirmCallback, abortedCallback) => {
-  if (confirmCallback && typeof confirmCallback !== "function") return;
-  if (abortedCallback && typeof abortedCallback !== "function") return;
+  if (!confirmCallback && typeof confirmCallback !== "function") return;
+  if (!abortedCallback && typeof abortedCallback !== "function") return;
 
   const confirmAction = () => {
     if (confirm(msg)) {
