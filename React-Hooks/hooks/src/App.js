a9e8991b58c3f7dac30a3127fca40a8f50dedeed
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 19:30:43 +0900

    useBeforeLeave

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
+const useBeforeLeave = (onBefore) => {
+  if (!onBefore && typeof onBefore !== "function") return;
+  const handle = (event) => {
+    const { clientY } = event;
+    if (0 >= clientY) {
+      onBefore();
+    }
   };
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
 
-  return { enablePrevent, disablePrevent };
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
 };
 
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const begForLife = () => console.log("Plz dont leave");
+  useBeforeLeave(begForLife);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={enablePrevent}>protect</button>
-      <button onClick={disablePrevent}>unProtect</button>
     </div>
   );
 };
