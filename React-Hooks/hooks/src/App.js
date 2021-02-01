007bd1a2ac483781c5d9859ffcde25b68646d9ac
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 20:21:13 +0900

    useNetwork

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -1,27 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-const useBeforeLeave = (onBefore) => {
-  if (!onBefore && typeof onBefore !== "function") return;
-  const handle = (event) => {
-    const { clientY } = event;
-    if (0 >= clientY) {
-      onBefore();
+const useNetwork = (onChange) => {
+  const [state, setState] = useState(navigator.online);
+  const handleChange = () => {
+    if (typeof onChange === 'function') {
+      onChange(navigator.online);
     }
+    setState(navigator.online);
   };
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
 
-    return () => document.removeEventListener("mouseleave", handle);
+    return () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
   }, []);
+  return state;
 };
 
 const App = () => {
-  const begForLife = () => console.log("Plz dont leave");
-  useBeforeLeave(begForLife);
+  const handleNetworkChage = online => {
+    console.log(online ? "online" : "offline");
+  };
+  var online = useNetwork(handleNetworkChage);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{online ? "Online" : "Offline"}</h1>
     </div>
   );
 };
