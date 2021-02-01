7ca409db95c8319587d54fab86baad18299419e3
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 15:00:29 +0900

    useState()

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decreasementItem = () => setItem(item - 1);
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increse</button>
+      <button onClick={decreasementItem}>descrese</button>
+    </div>
   );
+};
+
+// compare
+
+class AppUgly extends React.Component {
+  state = {
+    item: 1
+  };
+
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>Increse</button>
+        <button onClick={this.decreasementItem}>descrese</button>
+      </div>
+    );
+  }
+
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decreasementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
 }
 
 export default App;
