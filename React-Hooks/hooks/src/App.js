58ecb7a3e24b3e486ce2b1ce83d3d0ee0d5c361d
kr-MATAGI <dev_matagi@kakao.com>
2021-02-01 15:14:30 +0900

    useInput

--- React-Hooks/hooks/src/App.js
+++ React-Hooks/hooks/src/App.js
@@ -1,53 +1,32 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const useInput = (initValue, validator) => {
+  const [value, setValue] = useState(initValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decreasementItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increse</button>
-      <button onClick={decreasementItem}>descrese</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
-
-// compare
-
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>Increse</button>
-        <button onClick={this.decreasementItem}>descrese</button>
-      </div>
-    );
-  }
-
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decreasementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-}
 
 export default App;
